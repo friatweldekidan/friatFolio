1fb3e222643956672e60fdc99d4e6edad3a2b47a
Friat Weldekidan <34029637+friatweldekidan@users.noreply.github.com>
2024-04-30 10:52:28 +0300

    Page.js Updated

--- app/page.js
+++ app/page.js
@@ -4,8 +4,9 @@ import React from "react";
 import { motion } from "framer-motion";
 
 export default function Home() {
+  
   return (
-    <div className="min-h-[379px] pt-12 bg-cover bg-center bg-no-repeat" style={{backgroundImage: `url('assets/img/bg.jpg')`}}>
+    <div className="min-h-[379px] pt-12 bg-cover bg-center bg-no-repeat" style={{backgroundImage: `url('/assets/img/bg.jpg')`}}>
       <div className="max-w-4xl mx-auto font-mono">
         <motion.p
           initial={{ opacity: 0 }}
@@ -13,7 +14,7 @@ export default function Home() {
           transition={{ duration: 7.0 }}
           className="text-center text-gray-800 text-4xl"
         >
-          Hello, my name is Friat Weldekidan.
+          Hello, I am Friat Weldekidan.
         </motion.p>    
           <motion.p
             initial={{ opacity: 0 }}
