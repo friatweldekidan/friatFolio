51cb57fda31ed7f44f145b4b9b29644c35f53ef8
Friat Weldekidan <34029637+friatweldekidan@users.noreply.github.com>
2024-05-02 11:25:15 +0300

    Page.js updated

--- app/page.js
+++ app/page.js
@@ -6,7 +6,7 @@ import { motion } from "framer-motion";
 export default function Home() {
   
   return (
-    <div className="min-h-[379px] pt-12  bg-cover bg-right sm:bg-contain bg-no-repeat" style={{backgroundImage: `url("assets/img/bg.jpg")`}}>
+    <div className="min-h-[379px] pt-12  bg-contain bg-right  bg-no-repeat" style={{backgroundImage: `url("assets/img/bg.jpg")`}}>
       <div className="max-w-4xl mx-auto font-mono">
         <motion.p
           initial={{ opacity: 0 }}
@@ -20,7 +20,7 @@ export default function Home() {
             initial={{ opacity: 0 }}
             animate={{ opacity: 1 }}
             transition={{ duration: 9.0 }}
-            className="text-3xl text-center text-gray-600 my-12"
+            className="text-4xl text-center text-gray-600 my-12"
           >
             Frontend Developer
           </motion.p>
@@ -30,7 +30,7 @@ export default function Home() {
             href="assets/file/Friat_Weldekidan_Resume.pdf"
             target="_blank"
             download="Friat_Weldekidan_Resume.pdf"
-            className="bg-gray-300 hover:bg-gray-500 text-gray-600 text-xl font-bold py-4 px-6 rounded-[8px]"
+            className="bg-gray-300 hover:bg-gray-500 text-gray-600 text-xl  font-bold py-4 px-6 rounded-[8px]"
           >
             Download CV
           </a>
