e9f19934d0ca976eda9aa8d572c85d5af177aac7
Friat Weldekidan <34029637+friatweldekidan@users.noreply.github.com>
2024-05-15 10:01:40 +0300

    page.js updated

--- app/page.js
+++ app/page.js
@@ -6,13 +6,13 @@ import { motion } from "framer-motion";
 export default function Home() {
   
   return (
-    <div className="min-h-[379px] pt-12  bg-contain bg-right  bg-no-repeat" style={{backgroundImage: `url("assets/img/bg.jpg")`}}>
-      <div className="max-w-4xl mx-auto font-mono">
+    <div className="min-h-[379px] pt-12  bg-contain bg-right bg-no-repeat" style={{backgroundImage: `url("assets/img/bg.jpg")`}}>
+      <div className="max-w-3xl ml-[220px] font-mono">
         <motion.p
           initial={{ opacity: 0 }}
           animate={{ opacity: 1 }}
           transition={{ duration: 7.0 }}
-          className="text-center text-gray-800 text-4xl"
+          className="text-center text-gray-800 text-3xl"
         >
           Hello, I am Friat Weldekidan
         </motion.p>    
@@ -20,7 +20,7 @@ export default function Home() {
             initial={{ opacity: 0 }}
             animate={{ opacity: 1 }}
             transition={{ duration: 9.0 }}
-            className="text-4xl text-center text-gray-600 my-12"
+            className="text-3xl text-center text-gray-600 my-12"
           >
             Frontend Developer
           </motion.p>
